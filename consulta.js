b1b925db107afa7f059ae6ff5763f81a2929b21b
Luiz Silva <luizsilva.ee@gmail.com>
2020-08-24 22:18:25 -0300

    Adicionada descrição dos repositórios

--- consulta.js
+++ consulta.js
@@ -1,28 +1,52 @@
-const bodyElement = document.querySelector('body')
-const listElement = document.querySelector('#lista ul')
-const inputElement = document.querySelector('input')
-const buttonElement = document.querySelector('button')
+const bodyElement = document.querySelector("body");
+const containerElement = document.querySelector("#container-repos");
+const listElement = document.querySelector("#lista ul");
+const inputElement = document.querySelector("input");
+const buttonElement = document.querySelector("button");
 
-function busca(){
-  listElement.innerHTML = ''
-  let user = inputElement.value
-  axios.get('https://api.github.com/users/'+user+'/repos')
+function busca() {
+  listElement.innerHTML = "";
+  let user = inputElement.value;
+  axios
+    .get("https://api.github.com/users/" + user + "/repos")
     .then((response) => {
-      repos = response.data
-      for (repo of repos){
-        let repoElement = document.createElement('li')
-        let linkElement = document.createElement('a')
-        linkElement.setAttribute('href', repo.html_url)
-        linkElement.setAttribute('target', '_blank')
-        linkElement.text = repo.name
+      containerElement.setAttribute("class", "container");
+      const titleResponse = document.createElement("h1");
+      const textTitleResponse = document.createTextNode("Repositórios");
 
-        repoElement.appendChild(linkElement)
-        listElement.appendChild(repoElement)
+      titleResponse.appendChild(textTitleResponse);
+      listElement.appendChild(titleResponse);
+
+      repos = response.data;
+      for (repo of repos) {
+        let repoElement = document.createElement("li");
+        let linkElement = document.createElement("a");
+        let paragraphElement = document.createElement("p");
+
+        if (repo.description == null) {
+          repo.description = "Repositório sem descrição";
+        }
+
+        let descriptionElement = document.createTextNode(repo.description);
+
+        repoElement.setAttribute("onclick", "openRepo(repo.html_url)");
+        linkElement.setAttribute("href", repo.html_url);
+        linkElement.setAttribute("target", "_blank");
+        linkElement.text = repo.name;
+
+        repoElement.appendChild(linkElement);
+        paragraphElement.appendChild(descriptionElement);
+        repoElement.appendChild(paragraphElement);
+        listElement.appendChild(repoElement);
       }
     })
     .catch((error) => {
-      console.warn(error)
-    })
+      console.warn(error);
+    });
 }
 
-buttonElement.onclick = busca
+function openRepo(repo_url) {
+  window.open(repo_url, "_blank");
+}
+
+buttonElement.onclick = busca;
